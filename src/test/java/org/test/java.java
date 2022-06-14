d1914635484ba254b625757a5c7785acf0563925
sangeetha <sangeethaselvaraj26118@gmail.com>
2022-06-14 14:56:54 +0530

    Sangii code

--- src/test/java/org/test/java.java
+++ src/test/java/org/test/java.java
@@ -4,6 +4,8 @@ public class java{
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
 	}
 }
 	
