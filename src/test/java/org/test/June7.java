e98c9e50ff72671f48639b8115dc6aafde968e21
sangeetha <sangeethaselvaraj26118@gmail.com>
2022-06-14 14:18:50 +0530

    Project Structure

--- src/test/java/org/test/June7.java
+++ src/test/java/org/test/June7.java
@@ -3,8 +3,7 @@ package org.test;
 public class June7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
--- src/test/java/org/test/java.java
+++ src/test/java/org/test/java.java
@@ -3,6 +3,7 @@ package org.test;
 public class java{
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test1");
 	}
 }
 	
